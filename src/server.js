bb813affed881b5c6e150ff1dd2254dc52c64d5e
Ryan Di <ryan.weihao.di@gmail.com>
2021-02-26 18:43:26 +1100

    init

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+	res.send('Hello!');
+});
+
+app.post('/hello', (req, res) => {
+	res.send(`Hello, ${req.body.name}!`);
+});
+
+app.listen(8000, () => {
+	console.log('Listening on port 8000');
+});
